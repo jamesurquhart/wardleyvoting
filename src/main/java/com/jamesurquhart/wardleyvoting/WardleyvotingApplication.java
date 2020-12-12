214c9e98da94a8063fe5ae01c7cd88ea2500ca2e
James Urquhart <jurquhart@pivotal.io>
2020-12-12 11:19:12 -0800

    First commit

---
+++ src/main/java/com/jamesurquhart/wardleyvoting/WardleyvotingApplication.java
@@ -0,0 +1,13 @@
+package com.jamesurquhart.wardleyvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardleyvotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardleyvotingApplication.class, args);
+	}
+
+}
